532c0741dc41a585993fac72f023f327d293b649
Joao Paulo Alencar Rodrigues <joaopauloalencarrodrigues@gmail.com>
2024-04-14 23:11:01 -0300

    adds third section

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@
 
 import React from "react";
 import Image from "next/image"
+import { motion } from "framer-motion"
 import { HeroParallax } from "@/components/ui/hero-parallax";
 import products from "@/utils/products";
+import { LampContainer } from "@/components/ui/lamp";
+import { ParallaxScroll } from "@/components/ui/parallax-scroll";
 
 export default function HeroParallaxDemo() {
   return (
@@ -18,11 +21,36 @@ export default function HeroParallaxDemo() {
 
       <HeroParallax products={products} />
 
-      <div className="w-full mt-20 h-[60vh] relative bg-[url('https://upandup.agency/wp-content/uploads/2024/03/Cinematography-Sound-64-1024x682.jpg')]">
-        <div className="size-full bg-black/50 flex flex-col align-center justify-center">
-          <h1 className="text-2xl font-bold mx-4 w-fit h-fit">A agência que irá elevar o nível de seus eventos</h1>
-          <h2 className="mt-2 mx-4 w-fit h-fit">Do conceito à execução, nós transformamos sua visão em eventos extraordinários.</h2>
-        </div>
+      <div className="h-80 w-full bg-gradient-to-t from-slate-950 to-black to-60%" />
+      <LampContainer>
+        <motion.h1
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+        >
+          Onde a magia <br /> acontece
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-2 bg-slate-400 py-4 bg-clip-text text-center tracking-tight text-transparent md:text-2xl"
+        >
+          Transformando ideias em eventos que encantam e inspiram
+        </motion.p>
+      </LampContainer>
+
+      <div className="min-h-screen w-full bg-slate-950">
+        <ParallaxScroll images={products.map(item => item.thumbnail)} />
       </div>
     </main>
   )
