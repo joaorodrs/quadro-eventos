df4b7b7666bbdd566e1dd6b041b673be0e950655
Joao Paulo Alencar Rodrigues <joaopauloalencarrodrigues@gmail.com>
2024-04-19 22:18:36 -0300

    fix(config): fixes next config images

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   images: {
     domains: ['https://scontent.cdninstagram.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.cdninstagram.com',
+      },
+    ],
   },
 };
 
